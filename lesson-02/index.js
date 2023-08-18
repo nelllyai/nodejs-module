4230b2aca2fb1f86493e5bb193ecbfd49686adf9
Nelly Nikitina <worldpiece000@gmail.com>
2023-08-18 17:46:53 +0300

    feat: add lesson 2

---
+++ lesson-02/index.js
@@ -0,0 +1,8 @@
+const mpackage = require("mpackage");
+console.log(
+  mpackage({
+    name: "нелли никитина",
+    dateBirth: "07.06.2001",
+    purpose: "карьЕрныЙ роСт",
+  })
+);
---
+++ lesson-02/index.mjs
@@ -0,0 +1,8 @@
+import mpackage from "mpackage";
+console.log(
+  mpackage({
+    name: "нелли никитина",
+    dateBirth: "07.06.2001",
+    purpose: "карьЕрныЙ роСт",
+  })
+);
